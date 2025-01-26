c08f90fc3af1585fa03df65b706d2fe02dc87b7e
mlobeto <mercedeslobeto@gmail.com>
2025-01-26 14:13:59 -0300

    vite

--- BonitaFront/vite.config.js
+++ BonitaFront/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://demo1.taxxa.co', // URL de destino
+        target: 'http://localhost:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         configure: (proxy) => {
           proxy.on('proxyRes', (proxyRes) => {
-            
             delete proxyRes.headers['access-control-allow-origin'];
             proxyRes.headers['access-control-allow-origin'] = 'http://localhost:5173';
           });

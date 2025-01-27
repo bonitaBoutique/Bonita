67d0d0290f112a37a3c78d64e534ca870939a7f5
mlobeto <mercedeslobeto@gmail.com>
2025-01-27 15:54:02 -0300

    get/Get

--- BonitaBack/src/controller/ReservationController/index.js
+++ BonitaBack/src/controller/ReservationController/index.js
@@ -1,5 +1,5 @@
 const applyingPayments = require("./applyingPayments");
-const { getAllReservations } = require("./GetAllReservations");
+const { getAllReservations } = require("./getAllReservations");
 const { reservationByDocument } = require("./reservationByDocument");
 const { updateReservation } = require("./updateReservation");
 

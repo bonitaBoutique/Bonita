e1ac637fff555a115bc8cc0caa1d064b7115c8be
yanirc1981 <cerzurc@hotmail.com>
2025-02-14 17:29:37 -0300

    se agregan logos debajo de imagen y se acomoda ancho de imagen

--- BonitaFront/src/Components/LandingPrincipal.jsx
+++ BonitaFront/src/Components/LandingPrincipal.jsx
@@ -15,25 +15,45 @@ const LandingPrincipal = () => {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen bg-black">
+      <div className="min-h-screen bg-colorBeige">
         {/* Carrusel de banners */}
-        <div className="h-screen w-full overflow-hidden relative">
-          <div className="carousel h-full relative">
-            <img
-              src={banner3}
-              alt="Banner 1"
-              className="w-full h-full object-cover opacity-55"
-            />
+        <div className="flex justify-center items-center h-screen p-4">
+          <div className="w-full h-full max-w-screen-lg overflow-hidden relative rounded-lg">
+            <div className="carousel h-full relative">
+              <img
+                src={banner3}
+                alt="Banner 1"
+                className="w-full h-full mt-12 object-cover rounded-lg"
+              />
 
-            {/* Frase sobre la imagen */}
-            <div className="absolute top-1/2 left-8 transform -translate-y-1/2 text-rose-200">
-              <h1 className="text-4xl sm:text-5xl md:text-5xl font-extrabold leading-tight font-nunito">
-                ENCUENTRA EL MEJOR OUTFIT EN <br />
-                <span className="text-5xl sm:text-6xl md:text-8xl">
-                  BONITA BOUTIQUE
-                </span>
-              </h1>
+              {/* Frase sobre la imagen */}
+              <div className="absolute top-1/2 left-8 transform -translate-y-1/2 text-rose-200">
+                <h1 className="text-3xl sm:text-4xl md:text-4xl font-extrabold leading-tight font-nunito">
+                  ENCUENTRA EL MEJOR OUTFIT EN <br />
+                  <span className="text-3xl sm:text-5xl md:text-6xl">
+                    BONITA BOUTIQUE
+                  </span>
+                </h1>
+              </div>
             </div>
+          </div>
+        </div>
+
+        {/* Logos sin fondo debajo de la imagen principal */}
+        <div className="flex justify-center items-center py-4">
+          <div className="flex space-x-8 animate-marquee w-full overflow-hidden">
+          <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
+              <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
+              <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
+              <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
+              <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
+              <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
+              <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
+              <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
+              <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
+              <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
+              <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
+              <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
           </div>
         </div>
 
@@ -42,42 +62,25 @@ const LandingPrincipal = () => {
         </div>
 
         {/* Carrusel de logos de pago */}
-        <div className="bg-black py-12 overflow-hidden relative">
-  <div className="flex animate-infinite-scroll w-[300%]">
-    {/* First set of logos */}
-    <div className="flex space-x-24 mx-12 border-r border-gray-700 pr-12">
-      <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
-      <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
-      <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
-      <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
-      <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
-      <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
-    </div>
-    
-    {/* Second set */}
-    <div className="flex space-x-24 mx-12 border-r border-gray-700 pr-12">
-      <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
-      <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
-      <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
-      <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
-      <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
-      <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
-    </div>
-
-    {/* Third set */}
-    <div className="flex space-x-24 mx-12">
-      <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
-      <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
-      <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
-      <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
-      <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
-      <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
-    </div>
-  </div>
-</div>
-</div>
-      
-     
+        <div className="bg-black py-4 overflow-hidden relative">
+          <div className="flex animate-marquee w-full overflow-hidden">
+            <div className="flex space-x-8">
+              <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
+              <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
+              <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
+              <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
+              <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
+              <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
+              <img src={efecti} alt="Efecti" className="h-20 w-auto object-contain" />
+              <img src={logoAmeric} alt="American Express" className="h-20 w-auto object-contain" />
+              <img src={master} alt="Mastercard" className="h-20 w-auto object-contain" />
+              <img src={logoVisa} alt="Visa" className="h-20 w-auto object-contain" />
+              <img src={logosiste} alt="siste" className="h-20 w-auto object-contain" />
+              <img src={contraentrega} alt="contra" className="h-20 w-auto object-contain" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

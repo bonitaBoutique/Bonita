b63b13513e4859131cb462f872e69db5a099f505
mlobeto <mercedeslobeto@gmail.com>
2025-04-07 10:58:30 -0300

    cc

--- BonitaBack/src/controller/invoiceControllers.js
+++ BonitaBack/src/controller/invoiceControllers.js
@@ -83,7 +83,7 @@ const getLastInvoiceNumber = async (req, res) => {
     if (!lastInvoice) {
       return res.status(200).json({ 
         success: true,
-        nextInvoiceNumber: "5"
+        nextInvoiceNumber: "6"
       });
     }
 

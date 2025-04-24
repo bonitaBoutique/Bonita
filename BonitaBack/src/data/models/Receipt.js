a5d1d68d7858a733d2ae2074f5137b680771db10
mlobeto <mercedeslobeto@gmail.com>
2025-04-24 13:54:55 -0300

    amount2

--- BonitaBack/src/data/models/Receipt.js
+++ BonitaBack/src/data/models/Receipt.js
@@ -8,33 +8,34 @@ module.exports = (sequelize) => {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
-        allowNull: false, // Auto incremento, sin valor predeterminado
+        allowNull: false,
       },
       cashier_document: {
         type: DataTypes.STRING,
         allowNull: false,
         references: {
-          model: 'Users',
-          key: 'n_document'
-        }},
+          model: "Users",
+          key: "n_document",
+        },
+      },
       buyer_name: {
         type: DataTypes.STRING,
-        allowNull: false, // Nombre del comprador (obligatorio)
+        allowNull: false,
       },
       buyer_email: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          isEmail: true, // Validación de email
+          isEmail: true,
         },
       },
       buyer_phone: {
         type: DataTypes.STRING,
-        allowNull: true, // Teléfono del comprador (opcional)
+        allowNull: true,
       },
       total_amount: {
         type: DataTypes.FLOAT,
-        allowNull: false, // Monto total del recibo
+        allowNull: false,
       },
       payMethod: {
         type: DataTypes.ENUM(
@@ -45,16 +46,36 @@ module.exports = (sequelize) => {
           "Crédito",
           "Bancolombia",
           "Otro"
-        ),         
-        
+        ),
+        allowNull: false,
+      },
+      amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false, 
+      },
+      amount2: {
+        type: DataTypes.FLOAT,
+        allowNull: true, 
+      },
+      payMethod2: {
+        type: DataTypes.ENUM(
+          "Efectivo",
+          "Sistecredito",
+          "Addi",
+          "Tarjeta",
+          "Crédito",
+          "Bancolombia",
+          "Otro"
+        ),
+        allowNull: true, // <-- Opcional
       },
       date: {
         type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW, // Fecha de emisión del recibo
+        defaultValue: DataTypes.NOW,
       },
     },
     {
-      timestamps: true, // Manejo de createdAt y updatedAt
+      timestamps: true,
     }
   );
 };
